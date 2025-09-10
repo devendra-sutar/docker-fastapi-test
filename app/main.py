f4b9816536c1e604d26a8d811768dc62f69a4b35
devendra-sutar <167191447+devendra-sutar@users.noreply.github.com>
2025-09-10 12:48:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,35 +1,17 @@
-from typing import List
 from fastapi import FastAPI
-
-from app import services
-from app.schema import UserIn, BaseResponse, UserListOut
+from .services import get_users, create_user
+from .schema import User
 
 app = FastAPI()
 
-
 @app.get("/")
-async def index():
-    """
-    Index route for our application
-    """
-    return {"message": "Hello from FastAPI ;)"}
+def read_root():
+    return {"message": "Hello, Docker FastAPI!"}
 
+@app.get("/users")
+def read_users():
+    return get_users()
 
-@app.post("/users", response_model=BaseResponse)
-async def user_create(user: UserIn):
-    """
-    Add user data to json file
-    """
-    try:
-        services.add_userdata(user.dict())
-    except:
-        return {"success": False}
-    return {"success": True}
-
-
-@app.get("/users", response_model=UserListOut)
-async def get_users():
-    """
-    Read user data from json file
-    """
-    return services.read_usersdata()
+@app.post("/users")
+def add_user(user: User):
+    return create_user(user)
